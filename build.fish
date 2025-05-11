bcb612ee5ccbd393b947e6825a4c3d8835e6790f
Antonio Cheong <acheong@duti.dev>
2025-05-11 03:14:32 +0100

    use S3 storage instead

--- build.fish
+++ build.fish
@@ -1,3 +1,4 @@
 #!/usr/bin/fish
 gozer build
-set POD (kubectl get pod -l app=caddy -n statichost -o jsonpath='{.items[0].metadata.name}') ; kubectl exec -it -n statichost $POD -- sh -c 'rm -rf /tmp/build' && kubectl cp ./build/. $POD:/tmp/build -n statichost && kubectl exec -it -n statichost $POD -- sh -c 'rm -rf /srv/* && cp -r /tmp/build/* /srv/'
+# set POD (kubectl get pod -l app=caddy -n statichost -o jsonpath='{.items[0].metadata.name}') ; kubectl exec -it -n statichost $POD -- sh -c 'rm -rf /tmp/build' && kubectl cp ./build/. $POD:/tmp/build -n statichost && kubectl exec -it -n statichost $POD -- sh -c 'rm -rf /srv/* && cp -r /tmp/build/* /srv/'
+mcli mirror --overwrite --remove build r2/blog
